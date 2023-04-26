6eed2199475bd7be72c3fa107133f27ff34ac498
Rodrigo Araujo <rodhtml0@gmail.com>
2023-04-26 10:57:10 -0300

    Add files via upload

--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -1,72 +1,93 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography} from '@material-ui/core';
-import {Box} from '@mui/material';
-import { Link } from 'react-router-dom';
-import {useNavigate } from 'react-router-dom'
-import useLocalStorage from 'react-use-localstorage';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
 import './Navbar.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/TokensReducer';
+import { addToken } from '../../../store/tokens/Actions';
+
 function Navbar() {
-    const [token, setToken] = useLocalStorage('token');
-    let navigate = useNavigate();
-    
-    function goLogout(){
-        setToken('')
-        alert("Usuário deslogado")
-       navigate('/login')
+    let navigate = useNavigate()
+
+    const token = useSelector<TokenState, TokenState['tokens']>(
+        (state) => state.tokens
+    );
+
+    //dispara a ação para ser armazenada na store
+    const dispatch = useDispatch()
+
+    function goLogout() {
+        //seta o token para vazio, "adicionando" um valor vazio, fazendo com que as verificações
+        //nos outros componentes fiquem como false
+        dispatch(addToken(''))
+        alert('Deslogado com Sucesso')
+        navigate('/login')
     }
-    return (
-        <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Box className='cursor'>
-                        <Typography variant="h5" color="inherit">
+
+    var navbarComponent;
+
+    if(token!==''){
+       navbarComponent = <AppBar position="static" className='bg-color'>
+                <Toolbar variant="dense" className='container'>
+                    <Box className='itens'>
+                        <Typography variant="h5" color="inherit" className='cursor'>
                             BlogPessoal
                         </Typography>
                     </Box>
 
-                    <Box display="flex" justifyContent="start" >
-                        <Link to="/home" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
-                                    home
+                    <Box display="flex" justifyContent="center">
+                        <Link to='/home' className='text-decorator-none'>
+                            <Box mx={1} className='itens'>
+                                <Typography variant="h6" className='color-itens'>
+                                    Home
                                 </Typography>
                             </Box>
                         </Link>
-                        <Link to="/posts" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
-                                    postagens
+
+                        <Link to='/posts' className='text-decorator-none'>
+                            <Box mx={1} className='itens'>
+                                <Typography variant="h6" className='color-itens'>
+                                    Postagens
                                 </Typography>
                             </Box>
                         </Link>
-                        <Link to="/temas" className="text-decorator-none">
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                temas
+
+                        <Link to='/temas' className='text-decorator-none'>
+                            <Box mx={1} className='itens'>
+                                <Typography variant="h6" className='color-itens'>
+                                    Temas
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to='/formularioTema' className='text-decorator-none'>
+                            <Box mx={1} className='itens'>
+                                <Typography variant="h6" className='color-itens'>
+                                    Cadastrar Tema
+                                </Typography>
+                            </Box>
+                        </Link>
+
+
+                        <Box mx={1} className='itens' color='white' onClick={goLogout}>
+                            <Typography variant="h6" className='color-itens'>
+                                Logout
                             </Typography>
                         </Box>
-                        </Link>
-                        <Link to="/formularioTema" className="text-decorator-none">
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                        </Link>
-                      
-                            <Box mx={1} className='cursor' onClick={goLogout}>
-                                <Typography variant="h6" color="inherit">
-                                    logout
-                                </Typography>
-                            </Box>
-                        
+
                     </Box>
 
                 </Toolbar>
             </AppBar>
+    }
+
+    return (
+        <>
+            {navbarComponent}
         </>
-    )
+    );
 }
 
 export default Navbar;
